5d010ff83927de13180f524bebbf80062bcaf9fd
Sahil Bharti <sahil.884062@gmail.com>
2026-02-03 00:00:03 +0530

    update missing number code

--- Array/find_missingNumber.cpp
+++ Array/find_missingNumber.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+// Brute Force Approach
 int missingNumber(int arr[], int n)
 {
     for (int i = 1; i <= n; i++)
@@ -19,12 +20,61 @@ int missingNumber(int arr[], int n)
     return -1;
 }
 
+// Better Approach
+
+int betterMissingNumber(int arr[], int n)
+{
+    int hash[n + 1] = {0};
+    for (int i = 0; i < n - 1; i++)
+    {
+        hash[arr[i]] = 1;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (hash[i] == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+// Optimal Approachz
+
+// int OptimalMissingNumber(int arr[], int n)
+// {
+//     int total = n * (n + 1) / 2;
+//     int sum = 0;
+
+//     for (int i = 0; i < n-1; i++)
+//     {
+//         sum += arr[i];
+//     }
+//     return total - sum;
+// }
+
+int optimalMissingNumber(int arr[], int n)
+{
+    int xor1 = 0, xor2 = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        xor1 ^= i;
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        xor2 ^= arr[i];
+    }
+
+    return xor1 ^ xor2;
+}
+
 int main()
 {
     int arr[] = {1, 2, 4, 5};
     int n = 5;
 
-    cout << "Missing Number is: " << missingNumber(arr, n);
+    // cout << "Missing Number is: " << missingNumber(arr, n);
+    cout << "Missing Number is: " << optimalMissingNumber(arr, n);
 
     return 0;
 }
