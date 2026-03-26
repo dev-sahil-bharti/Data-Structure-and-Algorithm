e4808f6f23f9027e65171339c268ca8d2c804352
er-sahilbharti <sahil.884062@gmail.com>
2026-03-27 00:24:32 +0530

    update code bubble sort

--- Sorting Algorithms/bubbleSort.cpp
+++ Sorting Algorithms/bubbleSort.cpp
@@ -6,12 +6,19 @@ void bubbleSort(vector<int> &arr, int n)
 {
     for (int i = 0; i < n - 1; i++)
     {
+        bool isSwap = false;
+
         for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
+                isSwap = true;
             }
+        }
+        if (!isSwap) // Array is Already sorted
+        {
+            return;
         }
     }
 }
