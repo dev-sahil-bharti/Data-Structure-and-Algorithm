1fcf58873e57e442391c56705dccc5b08d2e3f9d
er-sahilbharti <sahil.884062@gmail.com>
2026-03-27 00:18:46 +0530

    bubble sort

---
+++ Sorting Algorithms/bubbleSort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bubbleSort(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr{4, 2, 1, 5, 3};
+    int n = arr.size();
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted Array is: ";
+    printArray(arr, n);
+
+    return 0;
+}
